422b2287de02ee8027807c1d64b81a38258a8237
Gia Hoang Nguyen <giahoang.nguyen@audiencemeida.com>
2016-03-19 08:31:32 +0700

    update

--- centos-symfony/root/start.sh
+++ centos-symfony/root/start.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-#echo "Starting SSHd..."
-#/usr/sbin/sshd
-#echo "Starting Nginx..."
-/usr/sbin/nginx
-#echo "Starting php-fpm..."
-/usr/sbin/php-fpm
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
